df274d3fa174d315c852ddd6a44bc3b917bb7b40
Daniel Rodrigo <Danielkrokovsky@hotmail.com>
2025-07-16 19:00:13 -0300

    commit

--- src/main/java/com/packt/aws/books/pipeline/DemoApplication.java
+++ src/main/java/com/packt/aws/books/pipeline/DemoApplication.java
@@ -3,10 +3,18 @@ package com.packt.aws.books.pipeline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import software.amazon.codeguruprofilerjavaagent.Profiler;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+		Profiler.builder().profilingGroupName("testetete").withHeapSummary(true) // optional - to start without heap
+																					// profiling, set to false or remove
+																					// line
+				.build().start();
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
